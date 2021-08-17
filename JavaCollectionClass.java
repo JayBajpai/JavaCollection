164be271ad36bcaed664ad0e3bd307e288a68e4f
Jay Bajpai <52155590+JayBajpai@users.noreply.github.com>
2021-08-17 13:55:01 +0530

    Welcome Message

---
+++ JavaCollectionClass.java
@@ -0,0 +1,10 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class JavaCollectionClass {
+	public static void main(String[] args) {
+		System.out.println("Welcome to master branch of Collections Class Programms");
+		
+	}
+}
